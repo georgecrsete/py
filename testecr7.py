a5912548b1895aea6685a062ac901193e4f62d47
George <george.p@escolar.ifrn.edu.br>
2025-09-10 17:56:42 -0300

    Update testecr7.py

--- testecr7.py
+++ testecr7.py
@@ -1,2 +1,12 @@
-n = input("seu nome")
-print(n)
+n = input("Seu nome")
+print("Olá," ,n) 
+
+n1 = int(input("digite um número:"))
+n2 = int(input("digite um número:"))
+soma = n1 + n2
+print(soma)
+
+n3 = int(input("digite um número:"))
+n4 = int(input("digite um número:"))
+media = (n3 +n4)/2
+print(media)
